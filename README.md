7e175071b01180abb20cfe0bf14290df6fb1a3e8
Bryan Bergo <bryan.l.bergo@gmail.com>
2023-01-30 12:15:21 -0800

    Spellcheck

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ A safety app inspired by Life360 for college students.
 ## Development Guidelines
 
 - Each feature should be developed on a separate branch
-- Each feature should be test thoroughly before merging with master
+- Each feature should be tested thoroughly before merging with master
 - Code should be well documented using comments
 
 ## Features
--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.green,
+        primaryColor: Colors.green.shade800
       ),
       home: const MyHomePage(title: 'Bronco Safe'),
     );
